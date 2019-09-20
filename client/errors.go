4f14f22fea3b2e5e08494b83c74e4b09d580ed2c
kigi <kigi.chang@gmail.com>
2019-09-20 15:08:31 +0800

    refactor error with new error feature in go 1.13.

--- client/errors.go
+++ client/errors.go
@@ -2,10 +2,11 @@ package client
 
 import (
 	"encoding/json"
+	"fmt"
 	"net"
 )
 
-// SawtoothError is the error defined in sawtooth resful api
+// SawtoothError is the error defined in sawtooth resful api.
 type SawtoothError struct {
 	Code    int32  `json:"code,omitempty"`
 	Title   string `json:"title,omitempty"`
@@ -13,34 +14,46 @@ type SawtoothError struct {
 }
 
 func (se *SawtoothError) Error() string {
-	return marshalToString(se)
+	return fmt.Sprintf(`{"code": %d, "title": %q, "message": %q}`, se.Code, se.Title, se.Message)
 }
 
-// Error ...
+// Error wraps Sawtooth error and http response code.
 type Error struct {
-	HTTPCode int            `json:"http_code,omitempty"`
-	Data     *SawtoothError `json:"data,omitempty"`
+	code int            // http response code
+	err  *SawtoothError // sawtooth error
+}
+
+// HTTPStatusCode returns http status code
+func (e *Error) HTTPStatusCode() int {
+	return e.code
+}
+
+// Unwrap implements error unwrap interface from go1.13
+func (e *Error) Unwrap() error {
+	return e.err
 }
 
 func (e *Error) Error() string {
-	return marshalToString(e)
+	return fmt.Sprintf(`{"http_code": %d, "error": %s}`, e.code, e.err.Error())
 }
 
 // NewError returns an error with http status code and sawtooth error.
 func NewError(httpCode int, raw []byte) error {
-	data := new(SawtoothError)
-	if err := json.Unmarshal(raw, data); err != nil {
+	se := new(SawtoothError)
+	if err := json.Unmarshal(raw, se); err != nil {
 		return err
 	}
 
 	return &Error{
-		HTTPCode: httpCode,
-		Data:     data,
+		code: httpCode,
+		err:  se,
 	}
 }
 
 // IsTimeError return err is a time out error or not
 func IsTimeError(err error) bool {
+	//x := &net.OpError{}
+	//return errors.As(err, &x) && x.Timeout()
 	v, ok := err.(net.Error)
 	return ok && v != nil && v.Timeout()
 }
