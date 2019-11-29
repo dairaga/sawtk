f00074d57e3dcf4cdecd2345e04fe9b9271b2f06
kigi <kigi.chang@gmail.com>
2019-11-29 13:29:28 +0800

    add teat wallet

---
+++ wallet/wallet_test.go
@@ -0,0 +1,18 @@
+package wallet
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	pubkey := `03511c83916ac338835b07f6b9f7c0aa10b7b427b48e16b5e91360c919c9cf60cb`
+
+	w, err := MakeFromHex(pubkey)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	ans := `SrLftu6GA4z6iV2RwiKCaf2xEHPW2ZCHT2ZbNGUeQXzVxQYpLtf`
+	if w != ans {
+		t.Errorf("wallet want %q, but %q", ans, w)
+	}
+}
